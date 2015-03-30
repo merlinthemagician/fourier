a8e92d93dd12c8049cfb8beb59e5b36c9ab98386
Ivo Siekmann <ivo.siekmann@unimelb.edu.au>
2015-03-30 15:51:05 +1100

    Added back-transformed Fouriers to analyseNoise.r

--- Rscripts/analyseNoise.r
+++ Rscripts/analyseNoise.r
@@ -1,3 +1,6 @@
 fourierNames=list.files(path="~/c/fourier/results",pattern="*Fourier.dat");
 
 fourierData=lapply(fourierNames,function(x) read.table(paste("~/c/fourier/results/",x, sep=""),sep="\t", colClasses="complex"));
+
+noiseNames=list.files(path="~/c/fourier/results",pattern="*[0-9].dat");
+noiseData=lapply(noiseNames,function(x) read.table(paste("~/c/fourier/results/",x, sep=""),sep="\t"));
