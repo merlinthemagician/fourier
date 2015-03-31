bc57e1e36b7b087924183bae60ecf74633968dc1
Ivo Siekmann <ivo.siekmann@unimelb.edu.au>
2015-03-31 17:22:13 +1100

    Extended analyseNoise.r by formulas from paper

--- Rscripts/analyseNoise.r
+++ Rscripts/analyseNoise.r
@@ -3,4 +3,17 @@ fourierNames=list.files(path="~/c/fourier/results",pattern="*Fourier.dat");
 fourierData=lapply(fourierNames,function(x) read.table(paste("~/c/fourier/results/",x, sep=""),sep="\t", colClasses="complex"));
 
 noiseNames=list.files(path="~/c/fourier/results",pattern="*[0-9].dat");
-noiseData=lapply(noiseNames,function(x) read.table(paste("~/c/fourier/results/",x, sep=""),sep="\t"));
+noiseData=lapply(noiseNames,function(x) read.table(paste("~/c/fourier/results/",x, sep=""),sep=" ", colClasses="numeric"));
+
+Sstat=function(eps, tau,lambda, k) {
+    return(eps/(tau*(1+lambda*lambda*k*k)))
+}
+
+cMuNu=function(lambda, hx, N, mu, nu) {
+    return(1-2*lambda^2/(hx^2) *(cospi(2*mu/N) + cospi(2*nu/N))-2)
+}
+
+SstatDisc=function(eps,tau,lambda, hx, N, mu, nu) {
+    return(eps/(tau*cMuNu(lambda,hx,N,mu,nu))
+}
+
